f1c6c9142822cef8176a59b8e18591fd71e00798
abidino1 <abidino@gmail.com>
2024-01-10 14:10:15 -0800

    EC2 part of created VPC

--- main.tf
+++ main.tf
@@ -7,22 +7,34 @@ resource "aws_instance" "public_instance" {
   ami           = var.ami
   instance_type = var.instance_type
   key_name      = aws_key_pair.autodeploy.key_name
+  subnet_id              = aws_subnet.my_subnet.id
+  vpc_security_group_ids = [aws_security_group.ssh_access.id]
+
 
   tags = {
     Name = var.name_tag
   }
 }
 
-# New EBS volume configuration
-resource "aws_ebs_volume" "extra_volume" {
- availability_zone = "${aws_instance.public_instance.availability_zone}"
- size              = 10  # Size of the volume in GB
- type              = "gp2"  # General Purpose SSD
+provider "aws" {
+  region = var.aws_region
 }
 
-# Attach the new volume to the EC2 instance
-resource "aws_volume_attachment" "extra_volume_attachment" {
- device_name = "/dev/sdh"
- volume_id   = "${aws_ebs_volume.extra_volume.id}"
- instance_id = "${aws_instance.public_instance.id}"
+resource "aws_vpc" "my_vpc" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "MyVPC"
+  }
+}
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = var.subnet_cidr
+  availability_zone = var.availability_zone
+
+  tags = {
+    Name = "MySubnet"
+  }
 }
