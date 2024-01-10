b632d7f1fcc3949c1d2ddcbe5c29283f91a5ff34
abidino1 <abidino@gmail.com>
2024-01-10 00:07:25 -0800

    adding security group to EC2 instance

--- main.tf
+++ main.tf
@@ -6,10 +6,39 @@ resource "aws_key_pair" "autodeploy" {
 resource "aws_instance" "public_instance" {
   ami           = var.ami
   instance_type = var.instance_type
+  vpc_security_group_ids = [aws_security_group.ssh_access_sg.id]
 
   tags = {
     Name = var.name_tag,
   }
   
-  key_name = aws_key_pair.autodeploy.key_name  # Link the key pair to the instance
+  key_name = aws_key_pair.autodeploy.key_name  # Link the key pair to the instanc
 }
+
+#Create security group 
+resource "aws_security_group" "ssh_access_sg" {
+  name        = "ssh_access_sg"
+  description = "Open ports 23 to team members IPs"
+
+  #Allow incoming TCP requests on port 22 from team members public IPs
+  ingress {
+    description = "Incoming SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["24.130.157.178","67.164.82.76"]
+  }
+
+  #Allow all outbound requests
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ssh_access_sg"
+  }
+}
+
