c3e34da72f998211ff92cb302e2d88f7a2fef5e9
abidino1 <abidino@gmail.com>
2024-01-10 14:10:40 -0800

    adding variables

--- variables.tf
+++ variables.tf
@@ -40,3 +40,19 @@ variable "team_member_ips" {
  type        = list(string)
  default     = ["24.130.157.178/32", "67.167.82.76/32"]  # team members public IPs
 }
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_cidr" {
+  description = "CIDR block for the subnet"
+  default     = "10.0.1.0/24"
+}
+
+variable "availability_zone" {
+  description = "Availability Zone for the subnet"
+  default     = "us-east-1a"
+}
+
