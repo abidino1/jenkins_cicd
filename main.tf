43fc05949353cd9a9c843d9fa964690970d36a1a
abidino1 <abidino@gmail.com>
2024-01-10 17:37:07 -0800

    code line delete

--- main.tf
+++ main.tf
@@ -8,8 +8,6 @@ resource "aws_instance" "public_instance" {
   instance_type = var.instance_type
   key_name      = aws_key_pair.autodeploy.key_name
   subnet_id              = aws_subnet.my_subnet.id
-  vpc_security_group_ids = [aws_security_group.ssh_access.id]
-
 
   tags = {
     Name = var.name_tag
