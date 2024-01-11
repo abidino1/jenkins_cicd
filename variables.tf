054987b730e07bdbdb68edce827277c202dd9aca
abidino1 <abidino@gmail.com>
2024-01-10 18:02:41 -0800

    changing CIDR blocks for VPC & subnet

--- variables.tf
+++ variables.tf
@@ -43,12 +43,12 @@ variable "team_member_ips" {
 
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
-  default     = "10.0.0.0/16"
+  default     = "10.10.0.0/16"
 }
 
 variable "subnet_cidr" {
   description = "CIDR block for the subnet"
-  default     = "10.0.1.0/24"
+  default     = "10.10.10.0/24"
 }
 
 variable "availability_zone" {
