430020c8ebb4421d866283e2ec8967bcb40ed4c5
abidino1 <abidino@gmail.com>
2024-01-11 01:50:24 -0800

    code to output AWS private key

--- main.tf
+++ main.tf
@@ -1,31 +1,40 @@
-resource "aws_key_pair" "autodeploy" {
-  key_name   = "new_autodeploy_key"  # Set a unique name for your key pair
-  public_key = file("/var/jenkins_home/.ssh/id_rsa.pub")
+resource "aws_key_pair" "TF_key" {
+  key_name   = "TF_key"
+  public_key = tls_private_key.rsa.public_key_openssh
 }
 
 resource "aws_instance" "public_instance" {
- ami           = var.ami
- instance_type = var.instance_type
- key_name      = aws_key_pair.autodeploy.key_name
+  ami           = var.ami
+  instance_type = var.instance_type
+  key_name      = aws_key_pair.TF_key.key_name
 
- tags = {
-   Name = var.name_tag
- }
+  tags = {
+    Name = var.name_tag
+  }
 
- connection {
-   type        = "ssh"
-   user        = "ubuntu"
-   private_key = file(var.private_key_path)
-   host        = self.public_ip
- }
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = tls_private_key.rsa.private_key_pem
+    host        = self.public_ip
+  }
 
- provisioner "remote-exec" {
-   inline = [
-     "sudo apt-get update",
-     "sudo apt-get install -y apache2",
-     "sudo systemctl start apache2",
-     "sudo systemctl enable apache2"
-   ]
- }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update",
+      "sudo apt-get install -y apache2",
+      "sudo systemctl start apache2",
+      "sudo systemctl enable apache2"
+    ]
+  }
 }
 
+resource "tls_private_key" "rsa" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "local_file" "TF-key" {
+  content  = tls_private_key.rsa.private_key_pem
+  filename = "tfkey"
+}
