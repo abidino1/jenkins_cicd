dfb8ee75181814e4aab20f1b7c93d7c573bc6741
abidino1 <abidino@gmail.com>
2024-01-10 17:19:29 -0800

    took out duplication on provider

--- main.tf
+++ main.tf
@@ -16,10 +16,6 @@ resource "aws_instance" "public_instance" {
   }
 }
 
-provider "aws" {
-  region = var.aws_region
-}
-
 resource "aws_vpc" "my_vpc" {
   cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
