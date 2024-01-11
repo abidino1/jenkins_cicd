fa3c0e7bf7bc26d44c50069c29af5c39093ae5ad
abidino1 <abidino@gmail.com>
2024-01-11 12:39:23 -0800

    Apache2 creation code

--- main.tf
+++ main.tf
@@ -1,40 +1,39 @@
-resource "tls_private_key" "rsa" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-resource "local_file" "TF-key" {
-  content  = tls_private_key.rsa.private_key_pem
-  filename = "tfkey"
-}
-
-resource "aws_key_pair" "TF_key" {
-  key_name   = "TF_key"
-  public_key = tls_private_key.rsa.public_key_openssh
+resource "aws_key_pair" "autodeploy" {
+  #key_name   = "new_autodeploy_key"  # Set a unique name for your key pair
+  public_key = file("/var/jenkins_home/.ssh/id_rsa.pub")
 }
 
 resource "aws_instance" "public_instance" {
   ami           = var.ami
   instance_type = var.instance_type
-  key_name      = aws_key_pair.TF_key.key_name
+  key_name      = aws_key_pair.autodeploy.key_name
+  subnet_id              = aws_subnet.my_subnet.id
 
   tags = {
     Name = var.name_tag
   }
+}
 
-  connection {
-    type        = "ssh"
-    user        = "ubuntu"
-    private_key = tls_private_key.rsa.private_key_pem
-    host        = aws_instance.public_instance.public_ip
+resource "aws_vpc" "my_vpc" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "MyVPC"
   }
+}
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = var.subnet_cidr
+  availability_zone = var.availability_zone
 
-  provisioner "remote-exec" {
+  tags = {
+    Name = "MySubnet"
+  }
+provisioner "remote-exec" {
     inline = [
       "sudo apt-get update",
       "sudo apt-get install -y apache2",
       "sudo systemctl start apache2"
-
-    ]
-  }
 }
