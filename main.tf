8342c2e4db7c33c0d295d02babc228f682c26a14
abidino1 <abidino@gmail.com>
2024-01-11 09:13:55 -0800

    corrections

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+resource "tls_private_key" "rsa" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "local_file" "TF-key" {
+  content  = tls_private_key.rsa.private_key_pem
+  filename = "tfkey"
+}
+
 resource "aws_key_pair" "TF_key" {
   key_name   = "TF_key"
   public_key = tls_private_key.rsa.public_key_openssh
@@ -16,25 +26,15 @@ resource "aws_instance" "public_instance" {
     type        = "ssh"
     user        = "ubuntu"
     private_key = tls_private_key.rsa.private_key_pem
-    host        = self.public_ip
+    host        = aws_instance.public_instance.public_ip
   }
 
   provisioner "remote-exec" {
     inline = [
       "sudo apt-get update",
       "sudo apt-get install -y apache2",
-      "sudo systemctl start apache2",
-      "sudo systemctl enable apache2"
+      "sudo systemctl start apache2"
+
     ]
   }
-}
-
-resource "tls_private_key" "rsa" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-resource "local_file" "TF-key" {
-  content  = tls_private_key.rsa.private_key_pem
-  filename = "tfkey"
 }
